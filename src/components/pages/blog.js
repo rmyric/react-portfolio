2be1c0753f3488c36fee97411b401fb8cb4ff262
rmyric <rmyric1@gmail.com>
2020-04-19 10:21:50 -0400

    updated

---
+++ src/components/pages/blog.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+class Blog extends Component  {
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Blog</h2>
+                
+                <div>
+                <Link to="/about-me">Read more about myself</Link>
+                </div>
+
+            </div>    
+        );
+    }
+}
+
+export default Blog 
